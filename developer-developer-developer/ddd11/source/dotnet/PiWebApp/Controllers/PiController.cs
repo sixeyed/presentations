1de6a558d50b7954b2050cd7ba1186d3995f39dd
stonemane <elton.stoneman@rabobank.com>
2016-11-02 09:08:25 +0000

    Add hostname to Pi page

--- developer-developer-developer/ddd11/source/dotnet/PiWebApp/Controllers/PiController.cs
+++ developer-developer-developer/ddd11/source/dotnet/PiWebApp/Controllers/PiController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using PiWebApp.Math;
@@ -20,7 +21,8 @@ namespace PiWebApp.Controllers
             {
                 DecimalPlaces = dp.Value,
                 Value = pi.ToString(),
-                ComputeMilliseconds = stopwatch.ElapsedMilliseconds
+                ComputeMilliseconds = stopwatch.ElapsedMilliseconds,
+                ComputeHost = Environment.MachineName
             };
 
             return View(model);
