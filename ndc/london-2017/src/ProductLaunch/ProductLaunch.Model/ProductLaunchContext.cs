fe1776c0b7b2fb2d985493564e67327c88596f11
Elton Stoneman <elton@sixeyed.com>
2017-01-11 12:13:14 +0000

    Initial sample project

---
+++ ndc/london-2017/src/ProductLaunch/ProductLaunch.Model/ProductLaunchContext.cs
@@ -0,0 +1,29 @@
+﻿using ProductLaunch.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductLaunch.Model
+{
+    public class ProductLaunchContext : DbContext
+    {
+        public ProductLaunchContext() : base("ProductLaunchDb") { }
+
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<Prospect> Prospects { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder builder)
+        {
+            builder.Entity<Country>().HasKey(c => c.CountryCode);
+            builder.Entity<Role>().HasKey(r => r.RoleCode);
+            builder.Entity<Prospect>().HasOptional<Country>(p => p.Country);
+            builder.Entity<Prospect>().HasOptional<Role>(p => p.Role);
+        }
+    }
+}
