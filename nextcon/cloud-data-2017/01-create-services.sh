657cb01324b4162d09667de87827a00fa64964e2
Elton Stoneman <elton@sixeyed.com>
2017-07-13 17:49:36 -0700

    Initial CDNext scripts

---
+++ nextcon/cloud-data-2017/01-create-services.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+docker network create -d overlay newsletter
+
+docker service create \
+ --network newsletter --endpoint-mode dnsrr \
+ --constraint 'node.platform.os == windows' \
+ --env ACCEPT_EULA=Y \
+ --env-file db-credentials.env \
+ --name db \
+ microsoft/mssql-server-windows-express
+
+docker service create \
+ --detach \
+ --network newsletter --endpoint-mode dnsrr \
+ --constraint 'node.platform.os == windows' \
+ --name elasticsearch \
+ sixeyed/elasticsearch:nanoserver
+
+docker service create \
+ --network newsletter --endpoint-mode dnsrr \
+ --constraint 'node.platform.os == windows' \
+ --name message-queue \
+ nats:0.9.6-nanoserver
+
+docker service create \
+ --network newsletter --endpoint-mode dnsrr \
+ --publish mode=host,target=80,published=80 \
+ --constraint 'node.platform.os == windows' \
+ --env-file db-credentials.env \
+ --name web \
+ dockersamples/signup-web
+
+docker service create \
+ --network newsletter --endpoint-mode dnsrr \
+ --constraint 'node.platform.os == windows' \
+ --env-file db-credentials.env \
+ --name save-handler \
+ dockersamples/signup-save-handler
+
+docker service create \
+ --network newsletter --endpoint-mode dnsrr \
+ --constraint 'node.platform.os == windows' \
+ --name index-handler \
+ dockersamples/signup-index-handler
+
+docker service create \
+ --network newsletter --endpoint-mode dnsrr \
+ --publish mode=host,target=5601,published=5601 \
+ --constraint 'node.platform.os == windows' \
+ --name kibana \
+ sixeyed/kibana:nanoserver
---
+++ nextcon/cloud-data-2017/remove-services.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker service rm db
+
+docker service rm elasticsearch 
+
+docker service rm message-queue 
+
+docker service rm web
+
+docker service rm save-handler 
+
+docker service rm index-handler
+
+docker service rm kibana 
+
+docker network rm newsletter
