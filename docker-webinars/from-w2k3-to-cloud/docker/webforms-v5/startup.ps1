83f678fb3d03bb79b57fbda6780ff86dff979071
Elton Stoneman <elton@sixeyed.com>
2018-06-27 15:12:09 +0100

    Add Docker W2K3 webinar

---
+++ docker-webinars/from-w2k3-to-cloud/docker/webforms-v5/startup.ps1
@@ -0,0 +1,38 @@
+function RedirectConfigFile {
+    param([string] $sourcePath, [string] $targetPath)
+
+    if ($sourcePath -And (Test-Path $sourcePath)) {
+    
+        Remove-Item -Force -Path $targetPath
+            
+        New-Item -Path $targetPath `
+                -ItemType SymbolicLink `
+                -Value $sourcePath
+
+        Write-Output "STARTUP: Redirected $targetPath config to read from $sourcePath"
+    }
+}
+
+Write-Output 'STARTUP: Loading config files'
+RedirectConfigFile $env:LOG4NET_CONFIG_PATH "$env:APP_ROOT\log4net.config"
+RedirectConfigFile $env:APPSETTINGS_CONFIG_PATH "$env:APP_ROOT\appSettings.config"
+RedirectConfigFile $env:CONNECTIONSTRINGS_CONFIG_PATH "$env:APP_ROOT\connectionStrings.config"
+       
+if ($env:DEPENDENCY_CHECK_ENABLED) {
+    Write-Output 'STARTUP: Validating dependencies'
+    & $env:APP_ROOT\DependencyChecker.exe
+    if ($LastExitCode -ne 0) {
+        return 1
+    }
+}
+
+Write-Output 'STARTUP: Starting IIS'
+Start-Service W3SVC
+Invoke-WebRequest http://localhost/ -UseBasicParsing | Out-Null
+
+Write-Output 'STARTUP: Starting metrics exporter'
+Start-Process -NoNewWindow -WorkingDirectory $env:METRICS_COLLECTOR_ROOT `
+              -FilePath "$env:METRICS_COLLECTOR_ROOT\aspnet-exporter.exe"
+
+Write-Output 'STARTUP: Tailing log'
+Get-Content -Path "C:\logs\WebFormsApp.log" -Tail 1 -Wait 
