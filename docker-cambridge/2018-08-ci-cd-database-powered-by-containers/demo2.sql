a6d7069274ae6553a217ce4a6f54deff1c983a58
Elton Stoneman <elton@sixeyed.com>
2018-10-10 19:32:24 +0100

    Add SQL CI/CD session

---
+++ docker-cambridge/2018-08-ci-cd-database-powered-by-containers/demo2.sql
@@ -0,0 +1,35 @@
+-- create database from scratch
+/* 
+CREATE DATABASE d2 
+ON (NAME='d2', FILENAME = 'C:\data\d2.mdf') 
+LOG ON (NAME='d2_log', FILENAME = 'C:\data\d2.ldf')
+/*
+
+-- deploy data
+/*
+use d2
+
+create table t1 (id nvarchar(20))
+
+insert into t1(id) values('a1')
+
+select * from t1
+*/
+
+-- check databases
+/*
+select name, physical_name from sys.master_files
+*/
+
+-- attach database
+/* 
+CREATE DATABASE d2 
+ON (FILENAME = 'C:\data\d2.mdf'), (FILENAME = 'C:\data\d2.ldf')
+FOR ATTACH
+/*
+
+/*
+use d2
+
+select * from t1
+*/
